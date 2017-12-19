4341bb61b29065d8563951bbddd12ee9ac8c8ae6
andreweggleston <andrew_eggleston@student.wayland.k12.ma.us>
2017-12-19 08:56:51 -0500

    Initial Commit

---
+++ src/brain_simulator/Main.java
@@ -0,0 +1,10 @@
+package brain_simulator;
+
+/**
+ * Created by student on 12/19/17.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/brain_simulator/video/Panel.java
@@ -0,0 +1,19 @@
+package brain_simulator.video;
+
+import javax.swing.*;
+import java.util.ArrayList;
+
+/**
+ * Created by student on 12/19/17.
+ */
+public class Panel extends JPanel {
+
+    ArrayList<String> phraseList;
+
+    public Panel(){
+
+    }
+
+
+
+}
